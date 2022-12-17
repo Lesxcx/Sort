4316e0b3110adba86c533a3b3320d4e6d3dde076
Dmytro Shcherbak <developerD76@ukr.net>
2022-12-17 14:41:33 +0100

    Bobble sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] tab = new int[]{1,5,2,3,9,4,2};
+        printTab(bobbleSort(tab));
+    }
+    private static void printTab(int[] tab) {
+        for(int elem: tab){
+            System.out.print(elem+ " ");
+        }
+    }
+    private static int[] bobbleSort(int[] tab) {
+        for(int i = 0; i < tab.length-1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < tab.length-1-i; j++) {
+                if (tab[j] > tab[j + 1]) {
+                    swapped = true;
+                    int temp = tab[j + 1];
+                    tab[j + 1] = tab[j];
+                    tab[j] = temp;
+                }
+            }
+            if(!swapped) return tab;
+        }
+        return tab;
     }
 }
