7cb7f22bce43653f6b276b26c1a7225fca7d328e
Dmytro Shcherbak <developerD76@ukr.net>
2022-12-17 15:54:24 +0100

    Insercion Sort

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         int[] tab = new int[]{1,5,2,3,9,4,2};
-        printTab(bobbleSort(tab));
+//        printTab(bobbleSort(tab));
+        printTab(insercionSort(tab));
     }
     private static void printTab(int[] tab) {
         for(int elem: tab){
@@ -21,6 +22,18 @@ public class Main {
                 }
             }
             if(!swapped) return tab;
+        }
+        return tab;
+    }
+
+    private static int[] insercionSort(int[] tab){
+        for (int i = 1; i<tab.length; i++){
+            int j;
+            int temp = tab[i];
+            for(j = i - 1; j >= 0 && tab[j] > temp; j--) {
+                tab[j + 1] = tab[j];
+            }
+            tab[j + 1] = temp;
         }
         return tab;
     }
